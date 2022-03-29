d5c83a5b599c281107532147f3b12ff45fe167c6
Tomasz Wawreniuk <tomaszwawreniuk@Tomaszs-MacBook-Pro-2.local>
2022-03-29 12:02:42 +0100

    Remove unnecessary stuff and add my stuff

--- home.nix
+++ home.nix
@@ -16,8 +16,6 @@ with builtins; {
       map (n: getAttrFromPath (splitString "." n) pkgs) (fromJSON (readFile ./pkgs.json));
 
     file = {
-      ".config/fish/fish_variables".source = fish/fish_variables;
-      ".config/fish/functions/fish_prompt.fish".source = fish/functions/fish_prompt.fish;
       "Library/Application Support/Code/User/settings.json".source = vscode/settings.json;
       "Library/Application Support/Code/User/keybindings.json".source = vscode/keybindings.json;
     };
@@ -37,70 +35,16 @@ with builtins; {
       enable = true;
     };
 
-    fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set -x NIX_PROFILES /nix/var/nix/profiles/default ~/.nix-profile
-        set -x NIX_PATH home-manager=${hm.path} nixpkgs=${pkgs.path}
-        set -p PATH ~/nix-home/bin ~/.local/bin /nix/var/nix/profiles/default/bin
-        set -x EDITOR vim
-      '';
-      shellAliases = {
-        hm = "run home-manager";
-        h = "history | fzy";
-        f = "fd | fzy";
-        ll = "exa -la --git";
-        cat = "bat -p --paging=never";
-        rgh = "rg -g '*.{hs}'";
-        rga = "rg -g '*.{art}'";
-        rgn = "rg -g '*.{nix}'";
-        rgs = "rg -g '*.{sql}'";
-        rgt = "rg -g '*.{tf}'";
-        gt = "git ls-tree -r --name-only HEAD 2>/dev/null | tree -C --fromfile";
-        gf = "git commit -a --fixup=(git rev-parse HEAD | cut -c 1-8)";
-        b = "git for-each-ref refs/heads --format='%(refname:short)' --sort='refname' | fzy --query \"$argv\" | xargs git checkout";
-        bm = "git checkout master";
-        bd = "git for-each-ref refs/heads --format='%(refname:short)' --sort='refname' | rg -v master | fzy --query \"$argv\" | xargs git branch -D";
-        p = "git push origin (git branch --show-current)";
-        pr = "gh pr create --fill";
-        ppr = "p && pr";
-      };
-    };
-
     git = {
       enable = true;
-      userName = "Zsolt Szende";
-      userEmail = "zsolt@artificial.io";
+      userName = "Tomasz Wawreniuk";
+      userEmail = "tomasz.wawreniuk@artificial.io";
       ignores = [
         ".DS_Store"
         "*.niu"
         ".local"
       ];
-      aliases = {
-        p = "pull -r --autostash";
-        co = "checkout";
-        c = "commit";
-        s = "status";
-        b = "branch";
-        d = "diff";
-        dh = "diff -- '*.hs'";
-        dn = "diff -- '*.nix'";
-        ds = "diff -- '*.sql'";
-        dt = "diff -- '*.tf'";
-      };
-      extraConfig = {
-        branch.autosetuprebase = "always";
-        push.recurseSubmodules = "no";
-        rebase.autosquash = "true";
-        submodule.recurse = "true";
-        delta.features = "side-by-side line-numbers";
-        delta.whitespace-error-style = "22 reverse";
-        core.pager = "delta";
-        interactive.diffFilter = "delta --color-only";
-      };
     };
-
-    htop.enable = true;
 
     jq.enable = true;
 
